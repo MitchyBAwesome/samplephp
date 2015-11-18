6d728522627181e1e5b1b19973732fec72115357
Mitch Beaumont <mitch.beaumont@rackspace.com>
2015-11-18 22:06:09 +1100

    Updated Home Page

--- views/welcome.php
+++ views/welcome.php
@@ -1,5 +1,13 @@
 <?php
 
-$output = "<h1>Hello, World!</h1>";
+$output = "
+<div class='row'>
+  <div class='col-md-4'></div>
+  <div class='col-md-4'><h1>Hello, World!</h1></div>
+  <div class='col-md-4'></div>
+</div>
+";
 
 return $output;
+
+
