1b1bfc67bc589d3cdb08605e4b6e3e7b69d6a17c
Mitch Beaumont <mitch.beaumont@rackspace.com>
2015-11-18 22:15:16 +1100

    Updated Home Page

--- views/welcome.php
+++ views/welcome.php
@@ -4,7 +4,7 @@ $output = "
 <div class='container-fluid'>
 	<div class='row'>
 	  <div class='col-md-4'></div>
-	  <div class='col-md-4'>Hello, World!</div>
+	  <div class='col-md-4'><p class='text-center'>Hello, World!</p></div>
 	  <div class='col-md-4'></div>
 	</div>
 </div>
